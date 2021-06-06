7e047f7dd5b804234af2baf2c54ff7974a719c74
Yuriy Horbatenko <xett03@gmail.com>
2021-06-05 23:55:12 -0400

    Heroku deploy test

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ import {readdirSync} from "fs";
 import cors from "cors";
 import mongoose from "mongoose";
 require("dotenv").config();
-const morgan = require("morgan");
 
+const morgan = require("morgan");
 const app = express();
 
 // db connection
-mongoose
-.connect(process.env.DATABASE, {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
@@ -24,9 +23,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 // route middleware
-readdirSync("./routes").map((r) => 
-    app.use("/api", require(`./routes/${r}`))
-);
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log("======== Server listening on " + port));
