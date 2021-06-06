e089eb6c00f65830242b98227dd16ac6442c06c6
Yuriy Horbatenko <xett03@gmail.com>
2021-06-06 01:09:38 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import {readdirSync} from "fs";
+import { readdirSync } from "fs";
 import cors from "cors";
 import mongoose from "mongoose";
+
 require("dotenv").config();
 
 const morgan = require("morgan");
@@ -12,12 +13,12 @@ mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
 })
-.then(() => console.log("Database Connected"))
-.catch((err) => console.log("Database Connection Error: ", err))
+.then(() => console.log("DB Connected"))
+.catch((err) => console.log("DB Connection Error: ", err));
 
-// middleware
+// middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
@@ -25,5 +26,5 @@ app.use(express.json());
 // route middleware
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log("======== Server listening on " + port));
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
