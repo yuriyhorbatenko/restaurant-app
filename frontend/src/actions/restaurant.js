e0378955e18b9544d8237848d5380480f26f52f6
Yuriy Horbatenko <xett03@gmail.com>
2021-06-16 23:38:18 -0400

    heroku fix

--- frontend/src/actions/restaurant.js
+++ frontend/src/actions/restaurant.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const allRestaurants = async () =>
-  await axios.get(`${process.env.REACT_APP_API}/restaurants`);
+  await axios.get(`/api/restaurants`);
 
 
 export const userRestaurants = async (token) =>
